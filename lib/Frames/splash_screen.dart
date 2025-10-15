a12de61f3ab828a232872a267a4e7b9cd1388a51
Gian003 <dev.Rodriguez2111@gmail.com>
2025-10-16 00:17:01 +0800

    Moving files and Creating frames

---
+++ lib/Frames/splash_screen.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'package:enquire/main.dart';
+
+class SplashScreenWrapper extends StatefulWidget {
+  @override
+  State<SplashScreenWrapper> createState() => _SplashScreenWrapperState();
+}
+
+class _SplashScreenWrapperState extends State<SplashScreenWrapper> {
+  bool _showSplash = true;
+
+  @override
+  void initState() {
+    super.initState();
+
+    Future.delayed(Duration(seconds: 3), () {
+      setState(() {
+        _showSplash = false;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedSwitcher(
+      duration: const Duration(milliseconds: 500),
+      child: _showSplash ? const SplashScreen() : const MyHomePage(),
+    );
+  }
+}
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+      body: Center(
+        child: Image.asset('assets/images/logo.png', width: 200, height: 200),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import 'package:enquire/Frames/splash_screen.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -19,55 +21,13 @@ class MyApp extends StatelessWidget {
             seedColor: const Color.fromRGBO(47, 88, 153, 100),
           ),
         ),
-        home: SplashScreenWrapper(),
+        home: SplashScreen(),
       ),
     );
   }
 }
 
 class MyAppState extends ChangeNotifier {}
-
-class SplashScreenWrapper extends StatefulWidget {
-  @override
-  State<SplashScreenWrapper> createState() => _SplashScreenWrapperState();
-}
-
-class _SplashScreenWrapperState extends State<SplashScreenWrapper> {
-  bool _showSplash = true;
-
-  @override
-  void initState() {
-    super.initState();
-
-    Future.delayed(Duration(seconds: 3), () {
-      setState(() {
-        _showSplash = false;
-      });
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return AnimatedSwitcher(
-      duration: const Duration(milliseconds: 500),
-      child: _showSplash ? const SplashScreen() : const MyHomePage(),
-    );
-  }
-}
-
-class SplashScreen extends StatelessWidget {
-  const SplashScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 255, 255, 255),
-      body: Center(
-        child: Image.asset('assets/images/logo.png', width: 200, height: 200),
-      ),
-    );
-  }
-}
 
 class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
