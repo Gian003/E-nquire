11cef4cef968c8b767d4db8d20bab4a85bf7bb99
Gian003 <dev.Rodriguez2111@gmail.com>
2025-11-05 23:31:16 +0800

    UI's

--- lib/Screen/home_screen.dart
+++ lib/Screen/home_screen.dart
@@ -6,8 +6,115 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Home')),
-      body: Center(child: Text('Welcome to E-nquire!')),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Image.asset('assets/images/logo.png', width: 75, height: 75),
+
+              const SizedBox(height: 30),
+
+              Align(
+                alignment: Alignment.centerLeft,
+                child: const Text(
+                  'Welcome,',
+                  style: TextStyle(
+                    fontFamily: 'Montserrat',
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+
+              const SizedBox(height: 30),
+
+              // Column(
+              //   children: [
+              //     SizedBox(
+              //       width: 325,
+              //       height: 110,
+              //       child: ElevatedButton(
+              //         style: ElevatedButton.styleFrom(
+              //           shape: RoundedRectangleBorder(
+              //             borderRadius: BorderRadius.horizontal(
+              //               left: Radius.circular(10),
+              //               right: Radius.circular(10),
+              //             ),
+              //           ),
+              //         ),
+              //         onPressed: () {},
+              //         child: Row(
+              //           children: [
+              //             Image.asset(
+              //               'assets/images/notificationpng',
+              //               height: 40,
+              //               width: 40,
+              //             ),
+
+              //             const SizedBox(width: 35),
+
+              //             Text(
+              //               'Notifications',
+              //               style: TextStyle(
+              //                 fontFamily: 'Montserrat',
+              //                 fontSize: 20,
+              //                 fontWeight: FontWeight.bold,
+              //                 color: Colors.black,
+              //               ),
+              //             ),
+              //           ],
+              //         ),
+              //       ),
+              //     ),
+              //   ],
+              // ),
+              SizedBox(
+                width: 325,
+                height: 110,
+                child: Column(
+                  children: [
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.horizontal(
+                            left: Radius.circular(10),
+                            right: Radius.circular(10),
+                          ),
+                        ),
+                      ),
+                      onPressed: () {},
+                      child: Row(
+                        children: [
+                          Image.asset(
+                            'assets/images/notification.png',
+                            height: 40,
+                            width: 40,
+                          ),
+
+                          const SizedBox(width: 20),
+
+                          Text(
+                            'Schedule',
+                            style: TextStyle(
+                              fontFamily: 'Montserrat',
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
